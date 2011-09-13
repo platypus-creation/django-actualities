4bf3c836651680c07271141e01923b0d74fe7600
Guillaume Esquevin <guillaume.esquevin@platypus-creation.com>
2011-09-13 16:17:53 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-actualities",
-    version = "0.2.1",
+    version = "0.2.2",
     description = """
     A django blog app
     """,
